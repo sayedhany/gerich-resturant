0318551173af9fc56ba371398588d3e2b018bd99
sayedhany <sayedh@mail.ru>
2022-12-19 15:59:44 +0200

    add about us section

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-
-const SubHeading = () => (
-  <div>
-    SubHeading
+import React from "react";
+import { images } from "../../constants";
+const SubHeading = (props) => (
+  <div style={{ marginButton: "1rem" }}>
+    <p className="p__cormorant">{props.title}</p>
+    <img src={images.spoon} alt="spoon" className="spoon__img" />
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 import "./Header.css";
 
 const Header = () => (
-  <header className="app_header app_wrapper section__padding">
-    <h1>sayed</h1>
+  <header className="app_header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new Flavour" />
+      <h1 className="app__header-h1">The key to Fine dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus{" "}
+      </p>
+      <button className="custom__button">Explor Menu</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </header>
 );
 
